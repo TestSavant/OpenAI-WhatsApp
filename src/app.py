2bddcfb3c46691bae36e1727e477e38dcf1f3325
TestSavant <124753965+TestSavant@users.noreply.github.com>
2023-06-17 03:21:48 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,23 +5,23 @@ from helper.twilio_api import send_message
 
 app = Flask(__name__)
 
-
+#Basic entrypoint. usefull to test the connection to the API
 @app.route('/')
 def home():
     return jsonify(
         {
             'status': 'OK',
-            'wehook_url': 'BASEURL/twilio/receiveMessage',
-            'message': 'Zenko AI webhook is ready. Next connect WhatsApp and Twilio',
+            'wehook_url_example': 'BASEURL/twilio/',
+            'message': 'Zenko AI webhooks are ready for work',
             
         }
     )
 
-
+#main Twilio endpoint. Use: https://BASE_URL/twilio  Used by WhatsApp to create a communication line with GPT 
 @app.route('/twilio', methods=['POST'])
 def receiveMessage():
     try:
-        # Extract incomng parameters from Twilio
+        # Extract incoming parameters from Twilio
         message = request.form['Body']
         sender_id = request.form['From']
 
@@ -32,3 +32,22 @@ def receiveMessage():
     except:
         pass
     return 'OK', 200
+
+
+# Additional API methods can be loaded here. As an example.. 
+
+    
+# @app.route('/image', methods=['POST'])
+# def receiveMessage():
+#     try:
+#         # Extract incomng parameters from GPT Image generator
+#         message = request.form['Body']
+#         sender_id = request.form['From']
+
+#         # Get response from Openai
+#         result = image(query)
+#         if result['status'] == 1:
+#             send_message(sender_id, result['response'])
+#     except:
+#         pass
+#     return 'OK', 200
