0e9c18b5fa2b1964b84cc19b33701aedba81ade4
TestSavant <124753965+TestSavant@users.noreply.github.com>
2023-06-28 02:24:36 -0700

    Update openai_api.py

--- helper/openai_api.py
+++ helper/openai_api.py
@@ -1,11 +1,14 @@
 import os
-
 import openai
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# Initialize the conversation history
+conversation_history = [
+    {'role': 'system', 'content': 'You are a helpful assistant bot named Zenko. You speak French first and English second. Always reply as Zenko'},
+]
 
 def chat_complition(prompt: str) -> dict:
     '''
@@ -16,13 +19,17 @@ def chat_complition(prompt: str) -> dict:
         - dict
     '''
     try:
+        # Add the user's message to the conversation history
+        conversation_history.append({'role': 'user', 'content': prompt})
+
         response = openai.ChatCompletion.create(
             model='gpt-4',
-            messages=[
-                {'role': 'system', 'content': 'You are a helpful assistant bot named Zenko. You speak French fist and English second. Always reply as Zenko'},
-                {'role': 'user', 'content': prompt},
-            ]
+            messages=conversation_history  # Use the conversation history
         )
+
+        # Add the model's response to the conversation history
+        conversation_history.append({'role': 'assistant', 'content': response['choices'][0]['message']['content']})
+
         return {
             'status': 1,
             'response': response['choices'][0]['message']['content']
