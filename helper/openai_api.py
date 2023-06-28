eb2191389d33361c8fcd8c5a6a938178a9602122
TestSavant <124753965+TestSavant@users.noreply.github.com>
2023-06-28 02:42:06 -0700

    Update openai_api.py

--- helper/openai_api.py
+++ helper/openai_api.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Initialize the conversation history
 conversation_history = [
-    {'role': 'system', 'content': 'You are a helpful assistant bot named Zenko. You speak French fist and English second. Always reply as Zenko.\n 
+    {'role': 'system', 'content': '''You are a helpful assistant bot named Zenko. You speak French fist and English second. Always reply as Zenko.\n 
 Generate a detailed AI Assistant dialogue script that guides a user through the onboarding process on the platform GoHighLevel. The dialogue should guide the user through the following steps:\n
 \n
 Onboarding customer flow, including joining the community, accessing support, and establishing launchpad connections.\n
@@ -19,7 +19,7 @@ Calendar Setup, encompassing the creation of a new calendar, selection of type (
 Phone Number Setup, which involves setting up the phone number with Twilio or a Lead connector, setting up call recording and call forwarding options, and setting up a whisper message.\n
 Reputation Management, including watching a video introduction, generating a link for reputation management, and setting up subdomains and a calendar page.\n
 Custom values setup, involving setting up custom values for business type, calendar URL, colors, company owner, social media URLs, lead email, lead phone, lead value, Twilio tracking number, and setting up a database reactivation workflow.\n
-Please also include a process for importing contact data, which includes importing a CSV file, adding contacts to a reactivation campaign, and template selection. Assume that the user consents to share the necessary information with the AI for setup.'},
+Please also include a process for importing contact data, which includes importing a CSV file, adding contacts to a reactivation campaign, and template selection. Assume that the user consents to share the necessary information with the AI for setup.'''},
 ]
 
 def chat_complition(prompt: str) -> dict:
